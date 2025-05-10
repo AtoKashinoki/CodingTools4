21ca86633e32d82810056d7771e3573c056a2bf1
AtoKashinoki <atokashinoki@gmail.com>
2025-05-10 18:59:13 +0900

    Update Execute block

--- src/CodingTools4/BasicStructure/__init__.py
+++ src/CodingTools4/BasicStructure/__init__.py
@@ -8,8 +8,8 @@ This package provides a mechanism for managing classes based on inheritance.\n
 
 
 if __name__ == '__main__':
-    from ..Error.Runtime import RuntimeError
-    raise RuntimeError(__file__)
+    from ..Error.Runtime import CannotRunError
+    raise CannotRunError(__file__)
 
 
 """ Imports """
