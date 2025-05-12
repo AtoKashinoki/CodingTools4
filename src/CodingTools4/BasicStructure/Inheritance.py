592aae8a3cf8b71ca34804347c352c721f853700
AtoKashinoki <atokashinoki@gmail.com>
2025-05-12 10:39:31 +0900

    Add Inheritance structure

---
+++ src/CodingTools4/BasicStructure/Inheritance.py
@@ -0,0 +1,28 @@
+""" BasicStructure.Inheritance
+This module provides the BasicStructure class, which assumes inheritance.\n
+このモジュールは継承を前提としたクラスのBasicStructureを提供します。
+"""
+
+
+""" Execute block """
+
+
+if __name__ == '__main__':
+    raise RuntimeError(f"Cannot run '{__file__}'")
+
+
+""" Imports """
+
+
+from abc import ABC
+
+
+"""
+    Basic structure
+"""
+
+
+class Inheritance(ABC):
+    """ Inheritance class structure """
+    ...
+
