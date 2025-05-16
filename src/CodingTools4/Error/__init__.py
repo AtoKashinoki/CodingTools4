37de08e5690277cc627333fa25fbedfc1eae8f4b
AtoKashinoki <atokashinoki@gmail.com>
2025-05-16 14:04:41 +0900

    Re add

--- src/CodingTools4/Error/__init__.py
+++ src/CodingTools4/Error/__init__.py
@@ -16,3 +16,12 @@ except ImportError as e:
     from .Import import PackageError
     Structure = PackageError(e)
     ...
+
+
+try:
+    from . import Import
+    ...
+except ImportError as e:
+    from .Import import PackageError
+    Import = PackageError(e)
+    ...
